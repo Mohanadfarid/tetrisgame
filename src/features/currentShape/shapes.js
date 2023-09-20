8998eac25da2c709ae23488e160af97dee0b5e9d
Mohanad Farid <106895779+Mohanadfarid@users.noreply.github.com>
2023-09-20 05:56:14 +0300

    modified the shapes and genrated random shapes at random rotation

--- src/features/currentShape/shapes.js
+++ src/features/currentShape/shapes.js
@@ -18,85 +18,237 @@ const red = colors[6];
 const black = colors[7];
 
 const cyanIBlock = [
+  //first form
   [
-    { isfull: true, color: cyan },
-    { isfull: true, color: cyan },
-    { isfull: true, color: cyan },
-    { isfull: true, color: cyan },
+    [
+      { isfull: true, color: cyan },
+      { isfull: true, color: cyan },
+      { isfull: true, color: cyan },
+      { isfull: true, color: cyan },
+    ],
+  ],
+  //second form
+  [
+    [{ isfull: true, color: cyan }],
+    [{ isfull: true, color: cyan }],
+    [{ isfull: true, color: cyan }],
+    [{ isfull: true, color: cyan }],
   ],
 ];
 
 const blueJBlock = [
+  //first form
   [
-    { isfull: true, color: blue },
+    [{ isfull: true, color: blue }],
+    [
+      { isfull: true, color: blue },
+      { isfull: true, color: blue },
+      { isfull: true, color: blue },
+    ],
   ],
+  //second form
   [
-    { isfull: true, color: blue },
-    { isfull: true, color: blue },
-    { isfull: true, color: blue },
+    [
+      { isfull: true, color: blue },
+      { isfull: true, color: blue },
+    ],
+    [{ isfull: true, color: blue }],
+    [{ isfull: true, color: blue }],
+  ],
+  // thered form
+  [
+    [
+      { isfull: true, color: blue },
+      { isfull: true, color: blue },
+      { isfull: true, color: blue },
+    ],
+    [
+      { isfull: false, color: black },
+      { isfull: false, color: black },
+      { isfull: true, color: blue },
+    ],
+  ],
+  //forth form
+  [
+    [
+      { isfull: false, color: black },
+      { isfull: true, color: blue },
+    ],
+    [
+      { isfull: false, color: black },
+      { isfull: true, color: blue },
+    ],
+    [
+      { isfull: true, color: blue },
+      { isfull: true, color: blue },
+    ],
   ],
 ];
 
 const orangeLBlock = [
+  //first form
   [
-    { isfull: true, color: orange },
-    { isfull: true, color: orange },
-    { isfull: true, color: orange },
+    [
+      { isfull: true, color: orange },
+      { isfull: true, color: orange },
+      { isfull: true, color: orange },
+    ],
+    [{ isfull: true, color: orange }],
   ],
+  //second form
   [
-    { isfull: true, color: orange },
+    [
+      { isfull: true, color: orange },
+      { isfull: true, color: orange },
+    ],
+    [
+      { isfull: false, color: black },
+      { isfull: true, color: orange },
+    ],
+    [
+      { isfull: false, color: black },
+      { isfull: true, color: orange },
+    ],
+  ],
+  // thered form
+  [
+    [
+      { isfull: false, color: black },
+      { isfull: false, color: black },
+      { isfull: true, color: orange },
+    ],
+    [
+      { isfull: true, color: orange },
+      { isfull: true, color: orange },
+      { isfull: true, color: orange },
+    ],
+  ],
+  //forth form
+  [
+    [{ isfull: true, color: orange }],
+    [{ isfull: true, color: orange }],
+    [
+      { isfull: true, color: orange },
+      { isfull: true, color: orange },
+    ],
   ],
 ];
 
 const yellowOBlock = [
   [
-    { isfull: true, color: yellow },
-    { isfull: true, color: yellow },
-  ],
-  [
-    { isfull: true, color: yellow },
-    { isfull: true, color: yellow },
+    [
+      { isfull: true, color: yellow },
+      { isfull: true, color: yellow },
+    ],
+    [
+      { isfull: true, color: yellow },
+      { isfull: true, color: yellow },
+    ],
   ],
 ];
 
 const greenSBlock = [
+  //first form
   [
-    { isfull: true, color: green },
+    [{ isfull: true, color: green }],
+    [
+      { isfull: true, color: green },
+      { isfull: true, color: green },
+    ],
+    [
+      { isfull: false, color: black },
+      { isfull: true, color: green },
+    ],
   ],
+  //second form
   [
-    { isfull: true, color: green },
-    { isfull: true, color: green },
-  ],
-  [
-    { isfull: false, color: black },
-    { isfull: true, color: green },
+    [
+      { isfull: false, color: black },
+      { isfull: true, color: green },
+      { isfull: true, color: green },
+    ],
+    [
+      { isfull: true, color: green },
+      { isfull: true, color: green },
+    ],
   ],
 ];
+
 const purpleTBlock = [
+  //first form
   [
-    { isfull: false, color: black },
-    { isfull: true, color: purple },
+    [
+      { isfull: false, color: black },
+      { isfull: true, color: purple },
+    ],
+    [
+      { isfull: true, color: purple },
+      { isfull: true, color: purple },
+    ],
+    [
+      { isfull: false, color: black },
+      { isfull: true, color: purple },
+    ],
   ],
   [
-    { isfull: true, color: purple },
-    { isfull: true, color: purple },
+    //second form
+    [
+      { isfull: false, color: black },
+      { isfull: true, color: purple },
+    ],
+    [
+      { isfull: true, color: purple },
+      { isfull: true, color: purple },
+      { isfull: true, color: purple },
+    ],
   ],
+  //thered form
   [
-    { isfull: false, color: black },
-    { isfull: true, color: purple },
+    [{ isfull: true, color: purple }],
+    [
+      { isfull: true, color: purple },
+      { isfull: true, color: purple },
+    ],
+    [{ isfull: true, color: purple }],
+  ],
+  //forth form
+  [
+    [
+      { isfull: true, color: purple },
+      { isfull: true, color: purple },
+      { isfull: true, color: purple },
+    ],
+    [
+      { isfull: false, color: black },
+      { isfull: true, color: purple },
+    ],
   ],
 ];
+
 const redZBlock = [
+  //first form
   [
-    { isfull: false, color: black },
-    { isfull: true, color: red },
+    [
+      { isfull: false, color: black },
+      { isfull: true, color: red },
+    ],
+    [
+      { isfull: true, color: red },
+      { isfull: true, color: red },
+    ],
+    [{ isfull: true, color: red }],
   ],
+  //second form
   [
-    { isfull: true, color: red },
-    { isfull: true, color: red },
-  ],
-  [
-    { isfull: true, color: red },
+    [
+      { isfull: true, color: red },
+      { isfull: true, color: red },
+    ],
+    [
+      { isfull: false, color: black },
+      { isfull: true, color: red },
+      { isfull: true, color: red },
+    ],
   ],
 ];
 
@@ -110,13 +262,24 @@ const shapes = {
   7: redZBlock,
 };
 
-export const randomShapeGenerator = () => { //func that generate random shape at random position to be dispatched
-    const shape=[[],[],[],[],[],[],[],[],[],[]]
-    const RowrandomShape=shapes[Math.floor(Math.random() * (7 - 1 + 1)) + 1]
-    const randomStartingIndex=Math.floor(Math.random() * (9-RowrandomShape.length - 0 + 1)) + 0;
-    for (let i = 0; i < RowrandomShape.length; i++) {
-        shape[randomStartingIndex + i]=RowrandomShape[i]
-    }
-    return shape
+export const randomShapeGenerator = () => {
+  //func that generate random shape at random position to be dispatched
+  const shape = [[], [], [], [], [], [], [], [], [], []];
+
+  const RowrandomShape = shapes[Math.floor(Math.random() * (7 - 1 + 1)) + 1];
+
+  const randomformIndex =
+    Math.floor(Math.random() * (RowrandomShape.length - 1 - 0 + 1)) + 0;
+  console.log(RowrandomShape);
+  console.log(randomformIndex);
+  const randomStartingIndex =
+    Math.floor(
+      Math.random() * (9 - RowrandomShape[randomformIndex].length - 0 + 1)
+    ) + 0;
+  for (let i = 0; i < RowrandomShape[randomformIndex].length; i++) {
+    shape[randomStartingIndex + i] = RowrandomShape[randomformIndex][i];
+  }
+  console.log(shape);
+  return shape;
 };
 // to do later create func to generate random shape with random positon and random rotate
