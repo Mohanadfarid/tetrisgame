eb16def04aa6c0fec1a569e795aff8a110c88518
Mohanad Farid <106895779+Mohanadfarid@users.noreply.github.com>
2024-01-16 17:03:51 +0200

    fixed some ui bugs

--- src/components/NextShap/NextShape.jsx
+++ src/components/NextShap/NextShape.jsx
@@ -1,23 +1,31 @@
-import React from 'react';
+import React from "react";
 import styles from "./nextShape.module.css";
-import Cell from '../cell/Cell';
-const NextShape = ({nextShape}) => {
-    return (
-        <div className={`${styles.nextShapContainer}`}>
-          {
-            nextShape.shape.shape[nextShape.shapeFormIndex].map((columnInfo,colIndex)=>{
-              let tempColumnInfo =JSON.parse(JSON.stringify(columnInfo)).reverse()
-              return <div style={{"width":`${100/4}%`}} className={`${styles.column}`} key={colIndex}>
-              {tempColumnInfo.map((cellInfo,cellIndex)=> <Cell numberOfCells={tempColumnInfo.length+2} cellInfo={cellInfo}/>
-              
-              )}
-              </div>
-            }
-            )
-            
-          }
-        </div> 
-    );
-}
+import Cell from "../cell/Cell";
+const NextShape = ({ nextShape }) => {
+  return (
+    <div className={`${styles.nextShapContainer}`}>
+      {nextShape.shape.shape[nextShape.shapeFormIndex].map(
+        (columnInfo, colIndex) => {
+          let tempColumnInfo = JSON.parse(JSON.stringify(columnInfo)).reverse();
+          return (
+            <div
+              style={{ width: `${100 / 4}%` }}
+              className={`${styles.column}`}
+              key={colIndex}
+            >
+              {tempColumnInfo.map((cellInfo, cellIndex) => (
+                <Cell
+                  key={cellIndex}
+                  numberOfCells={tempColumnInfo.length + 2}
+                  cellInfo={cellInfo}
+                />
+              ))}
+            </div>
+          );
+        }
+      )}
+    </div>
+  );
+};
 
 export default NextShape;
